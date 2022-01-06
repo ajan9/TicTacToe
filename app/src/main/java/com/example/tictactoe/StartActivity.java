3ae119f8b585047159843fde61d5fc0c9aaaa808
ivolarevic <46626092+ivolarevic@users.noreply.github.com>
2022-01-06 18:29:08 +0100

    TicTacToe winner colors

--- app/src/main/java/com/example/tictactoe/StartActivity.java
+++ app/src/main/java/com/example/tictactoe/StartActivity.java
@@ -1,19 +1,40 @@
 package com.example.tictactoe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class StartActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button[][] buttons = new Button[3][3];
+    int MAX_LENGTH = 3;
+    private Button[][] buttons = new Button[MAX_LENGTH][MAX_LENGTH];
     private boolean player1Turn = true;
     private int roundCount = 0;
+
+    static int color1ID, color2ID;
+    static boolean flag = false;
+
+    /*Kasnije promijeniti*/
+    private String player1Color = "blue";
+    private String player2Color = "orange";
+    private String defaultColor = "gray";
+
+    private CountDownTimer timer;
+    private static Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,29 +46,62 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
 
     public void initListeners(){
 
-        for (int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
+        for (int i = 0; i < MAX_LENGTH; i++){
+            for(int j = 0; j < MAX_LENGTH; j++){
                 String buttonID = "button" + i + j;
                 int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
                 buttons[i][j] = findViewById(resID);
                 buttons[i][j].setOnClickListener(this);
             }
         }
+        playerColor(player1Color, player2Color);
+    }
+
+    private void playerColor(String player1Color, String player2Color){
+        switch (player1Color){
+            case "blue":
+                color1ID = R.drawable.blue_btn;
+                break;
+            case "orange":
+                color1ID = R.drawable.orange_btn;
+                break;
+            case "gray":
+                color1ID = R.drawable.gray_btn;
+            break;
+        }
+
+        switch (player2Color){
+            case "blue":
+                color2ID = R.drawable.blue_btn;
+                break;
+            case "orange":
+                color2ID = R.drawable.orange_btn;
+                break;
+            case "gray":
+                color2ID = R.drawable.gray_btn;
+                break;
+        }
+
     }
 
     @Override
     public void onClick(View v) {
+
+        playerColor(player1Color, player2Color);
+
         if(!((Button)v).getText().toString().equals("")){
             return;
         }
 
         if(player1Turn){
-            ((Button)v).setBackground(getDrawable(R.drawable.blue_btn));
+            ((Button)v).setBackground(getDrawable(color1ID));
             ((Button)v).setText("X");
         }else{
-            ((Button)v).setBackground(getDrawable(R.drawable.orange_btn));
+            ((Button)v).setBackground(getDrawable(color2ID));
             ((Button)v).setText("O");
         }
+
+        roundCount++;
 
         if(checkForWin()){
             if(player1Turn){
@@ -63,24 +117,22 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-
-
     private boolean checkForWin(){
-        String[][] array = new String[3][3];
+        String[][] array = new String[MAX_LENGTH][MAX_LENGTH];
 
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
+        for(int i = 0; i < MAX_LENGTH; i++){
+            for(int j = 0; j < MAX_LENGTH; j++){
                 array[i][j] = buttons[i][j].getText().toString();
             }
         }
 
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < MAX_LENGTH; i++){
             if(array[i][0].equals(array[i][1]) && array[i][0].equals(array[i][2])
                     && !array[i][0].equals("")){
                 return true;
             }
         }
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < MAX_LENGTH; i++){
             if(array[0][i].equals(array[1][i]) && array[0][i].equals(array[2][i])
                     && !array[i][0].equals("")){
                 return true;
@@ -101,10 +153,13 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void player1Wins(){
+        showWinnerColors(color1ID);
         Toast.makeText(this,"Player 1 wins!", Toast.LENGTH_LONG).show();
         resetBoard();
     }
+
     private void player2Wins(){
+        showWinnerColors(color2ID);
         Toast.makeText(this,"Player 2 wins!", Toast.LENGTH_LONG).show();
         resetBoard();
     }
@@ -114,15 +169,35 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
         resetBoard();
     }
 
+    private void showWinnerColors(int color){
 
-    private void resetBoard(){
-        for(int i = 0; i < 3; i ++){
-            for(int j = 0; j < 3; j++){
-                buttons[i][j].setText("");
-                buttons[i][j].setBackground(getDrawable(R.drawable.gray_btn));
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            for (int j = 0; j < MAX_LENGTH; j++) {
+                buttons[i][j].setBackground(getDrawable(color));
             }
         }
-        player1Turn = true;
+
+        new CountDownTimer(2000, 50) {
+            @Override
+            public void onTick(long arg0) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                for (int i = 0; i < MAX_LENGTH; i++) {
+                    for (int j = 0; j < MAX_LENGTH; j++) {
+                        buttons[i][j].setBackground(getDrawable(R.drawable.gray_btn));
+                    }
+                }
+            }
+        }.start();
+
+    }
+
+    private void resetBoard(){
+        roundCount = 0;
+        player1Turn = !player1Turn;
     }
 
 
